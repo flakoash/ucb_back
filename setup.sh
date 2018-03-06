df58091c9c0639befbfe898b1e2b74cf0b453524
Juanpicr92 <jnpbl92@gmail.com>
2018-03-06 12:02:15 -0400

    configuracion final

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ sudo pip install virtualenv
 virtualenv myprojectenv
 source myprojectenv/bin/activate
 pip install django==1.8.18 pyhdb
+git submodule init
+git submodule update
 python pyhdb/setup.py install
 python django_hana_pyhdb/setup.py install
 sudo apt-get install libmysqlclient-dev
