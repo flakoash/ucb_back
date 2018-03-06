f87a887399941601ea60540763c14754b13171e2
Juanpicr92 <jnpbl92@gmail.com>
2018-03-06 11:52:59 -0400

    configuracion final

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-apt-get install pip 
-pip install virtualenv
+sudo apt-get install python-pip
+sudo pip install virtualenv
 virtualenv myprojectenv
 source myprojectenv/bin/activate
 pip install django==1.8.18 pyhdb
 python pyhdb/setup.py install
 python django_hana_pyhdb/setup.py install
-apt-get install libmysqlclient-dev
+sudo apt-get install libmysqlclient-dev
 pip install mysqlclient
 
--- ucb_back/settings.py
+++ ucb_back/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ltgi1s-$y+5_=97ki%h1gumw+68!$tt_$n*ji4$3kju&w44eu$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.18.63']
+ALLOWED_HOSTS = ['192.168.18.63', 'localhost']
 
 
 # Application definition
